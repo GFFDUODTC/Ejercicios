217669eb94c90265239e464d02ff4b7feb308ac6
GFFDUODTC <131828271+GFFDUODTC@users.noreply.github.com>
2023-05-31 22:35:14 -0600

    Add files via upload

---
+++ Mayuscula.java
@@ -0,0 +1,22 @@
+package Condicionales;
+
+import javax.swing.JOptionPane;
+
+public class Mayuscula {
+
+public static void main (String [] args){
+
+    char letra;
+     letra = JOptionPane.showInputDialog("Digite una letra: ").charAt(0);
+
+     if(Character.isUpperCase(letra)){
+
+         JOptionPane.showMessageDialog(null,"Es una letra mayuscula");
+     } else {
+
+         JOptionPane.showMessageDialog(null,"Es una letra minuscula");
+     }
+
+}
+
+}
