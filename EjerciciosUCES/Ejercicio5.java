5b52bc1a371276053319134338619fc7ceef1e79
GFFDUODTC <131828271+GFFDUODTC@users.noreply.github.com>
2023-06-05 21:34:32 -0600

    Add files via upload

---
+++ EjerciciosUCES/Ejercicio5.java
@@ -0,0 +1,26 @@
+//Escribí un programa que tome un array de Strings y concatene todas las palabras en una
+//sola cadena separada por espacios.
+
+
+package EjerciciosUCES;
+
+import java.util.ArrayList;
+
+public class Ejercicio5 {
+
+    public static void main (String []args){
+
+
+        ArrayList<String> Nombre = new ArrayList<String>();
+
+        Nombre.add("Miguel");
+        Nombre.add("Jose");
+        Nombre.add("Sebastian");
+        Nombre.add("Mauricio");
+
+        System.out.println(Nombre);
+
+
+    }
+
+}
