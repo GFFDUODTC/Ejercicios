570e74ba582ef6ba088f3cdbc959f7f29f7cf600
GFFDUODTC <131828271+GFFDUODTC@users.noreply.github.com>
2023-09-03 16:41:56 -0600

    Add files via upload

---
+++ Programacion1/Bucles/While.java
@@ -0,0 +1,21 @@
+package Programacion1.EjerciciosUCES.Bucles;
+import java.util.Scanner;
+public class While {
+
+    public static void main (String [] args){
+        Scanner entrada = new Scanner(System.in);
+
+        int i=1,contador;
+        System.out.print("Hasta que numero cuento?");
+        contador = entrada.nextInt();
+
+        while(i<=contador){
+            System.out.println(i);
+            i+=2; //El numero indica de cuanto en cuanto cuenta, en este caso cuenta de 2 en 2
+        }
+
+
+    }
+
+
+}
