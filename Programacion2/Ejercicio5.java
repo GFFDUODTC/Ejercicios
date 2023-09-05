e4646f7491b6d456a5226254ed79ef7f25aac6eb
GFFDUODTC <131828271+GFFDUODTC@users.noreply.github.com>
2023-09-05 19:30:34 -0300

    Add files via upload

---
+++ Programacion2/Ejercicio5.java
@@ -0,0 +1,51 @@
+
+//Adivinar un numero al azar del 1 al 10 , que tenga 3 intentos
+
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+
+
+
+
+
+
+
+public class Ejercicio5 {
+
+
+
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int numAleatorio = random.nextInt(10)+1;
+        for (int i = 0; i < 3;i++){
+        System.out.println("Ingrese un numero");
+        int numero = sc.nextInt();
+        if(numAleatorio==numero){
+            System.out.println("acerto " + numero);
+
+        } else {
+            System.out.println("No acerto "+ numero);
+        }
+
+
+
+
+    }
+
+
+
+
+
+    }
+
+
+
+
+}
