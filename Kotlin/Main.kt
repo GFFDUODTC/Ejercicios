2eb7303d5a291492941afe535005686f32b84959
Romer.s <131828271+Romer-s@users.noreply.github.com>
2024-04-01 21:19:48 -0600

    Add files via upload

---
+++ Kotlin/Main.kt
@@ -0,0 +1,19 @@
+/* * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz". */
+
+
+
+fun main() {
+
+    for (i in 1..100){
+        when {
+            i % 3 == 0 && i % 5 == 0 -> println("FizzBuzz")
+            i % 3 == 0 -> println("Fizz")
+            i % 5 == 0 -> println("Buzz")
+            else -> println(i)
+        }
+    }
+}
